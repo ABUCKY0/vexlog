d88da5d5e150a3d52fc4b1d436fe93ead547c882
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2024-04-23 19:01:47 -0500

    Update to pros 4 and rewrite

--- include/robotlog/robotlog.h
+++ include/robotlog/robotlog.h
@@ -11,106 +11,244 @@
 #include "api.h"  // Include PROS API header
 #include "colors.h"
 #include "pros/rtos.hpp"  // Include PROS RTOS header for Mutex and Task
+
+using namespace std;
+
 namespace LOG {
 
+
+// Enum representing log levels
 enum Level {
   DEBUG,    // Detailed information, typically of interest only when diagnosing
             // problems.
   INFO,     // Program Information.
   WARNING,  // Used in User Programs for if something unexpected but recoverable
-            // happened
+            // happened.
   ERROR,  // Useful for if the user program cannot recover, such as a sensor not
-          // being found
+          // being found.
 };
 
-class RobotLOG {
- private:
-  // pros::Mutex queueMutex;
-  // std::queue<std::string> logQueue;
-  // std::string logFILE;
-  // std::atomic<bool> running{true};
-  // pros::Task worker;
-  // std::atomic<bool> logstofile{false};
-  // std::atomic<Level> logLevel{INFO};
-
-  std::atomic<bool> isTakingNewLogs{
-      true};  // replaces running. I still want to finish flusing out the logs
-              // before stopping
-  std::atomic<bool> logstofile{false};
-  std::string logFilePath;
-  std::ofstream logFile;
-  std::queue<std::string> logQueue;  // queue of logs to be written to file
-  pros::Mutex logMutex;              // mutex to protect the queue
-  pros::Task worker;                 // task to write logs to file
-  /*
-   * Logging Level is as Follows:
-   * DEBUG: Detailed information, typically of interest only when diagnosing
-   * problems. Level 0 INFO: Program Information. Level 1 WARN: Used in User
-   * Programs for if something unexpected but recoverable happened ERROR: Useful
-   * for if the user program cannot recover, such as a sensor not being found
-   */
-  std::atomic<Level> loggingLevel{INFO};  // minimum level to log
+// Struct representing a log entry
+struct logEntry {
+  string file;  // Name of the file where the log is generated (__FILE__)
+  uint64_t microseconds;  // Timestamp in microseconds (Converted to Seconds in
+                          // getSeconds())
+  Level level;            // Log level (DEBUG, INFO, WARNING, ERROR)
+  string message;         // Log message
+  string func;            // Function where the log is generated (__FUNCTION__)
+  int line;               // Line number where the log is generated
 
   /**
-   * @brief Entry point for the background task that writes logs to the file
-   * @param param Pointer to the RobotLOG object
+   * Constructor for log entry
+   * @param file File where the log is generated
+   * @param level Log level
+   * @param message Log message
+   * @param func Function where the log is generated
+   * @param line Line number where the log is generated
+   * @example logEntry entry(__FILE__, Level::INFO, "Hello, World!", __FUNCTION__,
+   * __LINE__);
+   * @note It's reccomented to use @see log() macro to generate log entries
   */
-  static void taskEntry(void *param) {
-    RobotLOG *logger = static_cast<RobotLOG *>(param);
-    logger->processLogs();
+  logEntry(const std::string& file, Level level, const std::string& message,
+           const std::string& func, int line) {
+    this->file = file;
+    this->microseconds = pros::micros();
+    this->level = level;
+    this->message = message;
+    this->func = func;
+    this->line = line;
   }
 
   /**
-   * @brief Processes the logs in the queue and writes them to the file
+   * Get seconds from microseconds
+   * @return Seconds from the timestamp
   */
-  void processLogs() {
-    
+  int getSeconds() { return this->microseconds / 1000000; }
+
+  /**
+   * Get time in the format of MM:SS.mmm
+   * @return Time string in the format of MM:SS.mmm
+  */
+  string getTime() {
+    int total_seconds = this->microseconds / 1000000;
+    int minutes = total_seconds / 60;
+    int seconds = total_seconds % 60;
+    int milliseconds = (this->microseconds / 1000) % 1000;
+
+    string time = (minutes < 10 ? "0" : "") + std::to_string(minutes) + ":" +
+                  (seconds < 10 ? "0" : "") + std::to_string(seconds) + "." +
+                  (milliseconds < 100 ? "0" : "") +
+                  (milliseconds < 10 ? "0" : "") + std::to_string(milliseconds);
+
+    return time;
   }
 
-  static std::string levelToString(Level level) {
+  /**
+   * Colorize the log level
+   * @param level Log level
+   * @return Colorized log level
+  */
+  std::string colorizeLevel(Level level) {
     switch (level) {
       case DEBUG:
-        return Colors::GREEN + "DEBUG" + Colors::RESET;
+        return Colors::GREEN + "[DEBUG]" + Colors::RESET;
       case INFO:
-        return Colors::WHITE + "INFO" + Colors::RESET;
+        return Colors::WHITE + "[INFO]" + Colors::RESET;
       case WARNING:
-        return Colors::YELLOW + "WARN" + Colors::RESET;
+        return Colors::YELLOW + "[WARN]" + Colors::RESET;
       case ERROR:
-        return Colors::RED + "ERR" + Colors::RESET;
+        return Colors::RED + "[ERR]" + Colors::RESET;
       default:
         return "UNKNOWN";
     }
   }
+};
 
- public:
-  // No-arg constructor
-  RobotLOG() : worker(&RobotLOG::taskEntry, this) { logstofile = false; }
+/// @brief Class for logging
+class RobotLOG {
+ private:
+  std::atomic<bool> isTakingNewLogs{
+      true};  // Flag indicating whether the logger is taking new logs
+  std::atomic<bool> logstofile{
+      false};  // Flag indicating whether logs should be written to file
+  std::string logFilePath;        // Path to the log file
+  std::ofstream logFile;          // Output stream for writing logs to file
+  std::queue<logEntry> logQueue;  // Queue of logs to be written to file
+  pros::Mutex logMutex;           // Mutex to protect the log queue
+  pros::Task worker;              // Task to write logs to file asynchronously
+  std::string format = "%t - %l (%y-%b): %m";  // Log message format
 
-  RobotLOG(const std::string &logFileLoc) : worker(&RobotLOG::taskEntry, this) {
-    logstofile = true;
-    this->logFILE = "/usd/" + logFileLoc;
+  /**
+   * Static method to be called by the task scheduler to write logs to file
+   * @param param Pointer to the logger object
+   * @note This method is called by the task scheduler to write logs to file
+  */
+  static void taskEntry(void* param) {
+    RobotLOG* logger = static_cast<RobotLOG*>(param);
+    logger->processLogs();  // Process logs in the queue and write them to the
+                            // file
+  }
+  
+  /**
+   * Processes Queued Logs
+   * @note This method processes logs in the queue and writes them to the file
+  */
+  void processLogs() {
+    cout << ("Logger started\n");  // Log a message indicating that the logger
+                                   // has started
+    while (true) {
+      if (this->logQueue.empty()) {
+        pros::delay(30);  // Delay for 5 milliseconds if the queue is empty
+        continue;
+      }
+      logMutex.take(0);  // Take the mutex to access the log queue
+      logEntry message =
+          logQueue.front();  // Get the frontmost log entry from the queue
+      logQueue.pop();        // Remove the frontmost log entry from the queue
+
+      // Format the log message
+      std::string formattedMessage = format;
+      std::string time = message.getTime();
+      std::string level = message.colorizeLevel(message.level);
+      std::string file = message.file;
+      std::string func = message.func;
+      std::string log = message.message;
+      std::string line = std::to_string(message.line);
+
+      // Replace placeholders in the log message with actual values
+      std::size_t pos;
+      pos = formattedMessage.find("%t");
+      if (pos != std::string::npos) {
+        formattedMessage.replace(pos, 2, time);
+      }
+      pos = formattedMessage.find("%l");
+      if (pos != std::string::npos) {
+        formattedMessage.replace(pos, 2, level);
+      }
+      pos = formattedMessage.find("%y");
+      if (pos != std::string::npos) {
+        formattedMessage.replace(pos, 2, file);
+      }
+      pos = formattedMessage.find("%b");
+      if (pos != std::string::npos) {
+        formattedMessage.replace(pos, 2, func);
+      }
+      pos = formattedMessage.find("%m");
+      if (pos != std::string::npos) {
+        formattedMessage.replace(pos, 2, log);
+      }
+      pos = formattedMessage.find("%r");
+      if (pos != std::string::npos) {
+        formattedMessage.replace(pos, 2, line);
+      }
+      // if (logstofile && logFile.is_open()) {
+      //   logFile << formattedMessage << std::endl;
+      // }
+      cout << formattedMessage
+           << std::endl;  // Print the log message to the console
+      logMutex.give();
+      pros::delay(15);  // Delay for 15 milliseconds
+    }
   }
 
-  // Destructor
+ public:
+  /**
+   * Constructor for the logger
+  */
+  RobotLOG() : worker(&RobotLOG::taskEntry, this) { logstofile = false; }
+
+ /**
+  * Constructor for the logger
+  * @param logFileLoc Path to the log file
+ */
+  RobotLOG(const std::string& logFileLoc) : worker(&RobotLOG::taskEntry, this) {
+    logstofile = true;
+    this->logFilePath = "/usd/" + logFileLoc;
+    this->logQueue = std::queue<logEntry>();
+  }
+
+  /**
+   * Destructor for the logger
+  */
   ~RobotLOG() {
     shutdown();  // Ensure proper shutdown when object is destroyed
   }
 
+  /**
+   * Disable file logging
+  */
   void disableFileLogging() { logstofile = false; }
 
+  
+  /**
+   * Enable file logging
+  */
   void enableFileLogging() { logstofile = true; }
 
-  // Log method
+  /**
+   * Add a log entry to the queue
+   * @param level Log level
+   * @tparam message Log message
+   * @param file File where the log is generated
+   * @param function Function where the log is generated
+   * @param line Line number where the log is generated
+   * @example addlog(LOG::INFO, "Hello, World!", __FILE__, __FUNCTION__, __LINE__);
+   */
   template <typename T>
-  void log(Level level, const T &message) {}
+  void addlog(LOG::Level level, const T& message, const std::string& file,
+              const std::string& function, int line) {
+    std::ostringstream msgStream;
+    msgStream << message;               // Convert any type T to string
+    std::string msg = msgStream.str();  // Get the string from the stream
 
-  // Log method
-  template <typename T>
-  void log(const T &message) {}
+    this->logQueue.push(logEntry(file, level, msg, function, line));
+    cout << "Added log to queue" << endl;
+  }
 
-  // Shutdown method
+  /**
+   * Shutdown the logger
+  */
   void shutdown() {
-    running = false;  // Signal the background thread to stop
     if (worker.get_state() == pros::E_TASK_STATE_RUNNING) {
       worker.remove();  // Remove the task from the task scheduler
     }
@@ -118,5 +256,45 @@ class RobotLOG {
 };
 
 };  // namespace LOG
+
+/**
+ * Macro to generate log entries
+ * @param level Log level
+ * @param message Log message
+*/
+#define log(level, message) \
+  RobotLOG::addlog(level, message, __FILE__, __FUNCTION__, __LINE__)
+
+/**
+ * Macro to generate log entries with log level DEBUG
+ * @param message Log message
+ * @example debug("This is a debug message");
+ */
+#define info(message) \
+  RobotLOG::addlog(LOG::INFO, message, __FILE__, __FUNCTION__, __LINE__)
+
+/**
+ * Macro to generate log entries with log level INFO
+ * @param message Log message
+ * @example info("This is an info message");
+ */
+#define debug(message) \
+  RobotLOG::addlog(LOG::DEBUG, message, __FILE__, __FUNCTION__, __LINE__)
+
+/**
+ * Macro to generate log entries with log level WARNING
+ * @param message Log message
+ * @example warning("This is a warning message");
+ */
+#define warning(message) \
+  RobotLOG::addlog(LOG::WARNING, message, __FILE__, __FUNCTION__, __LINE__)
+
+/**
+ * Macro to generate log entries with log level ERROR
+ * @param message Log message
+ * @example error("This is an error message");
+ */
+#define error(message) \
+  RobotLOG::addlog(LOG::ERROR, message, __FILE__, __FUNCTION__, __LINE__)
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,9 @@
 
 using namespace LOG;
 using namespace std;
-RobotLOG logger("logger.txt");
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-  static bool pressed = false;
-  pressed = !pressed;
-  if (pressed) {
-    pros::lcd::set_text(2, "I was pressed!");
-  } else {
-    pros::lcd::clear_line(2);
-  }
-}
 
+
+  RobotLOG logger("logger.txt");
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -30,80 +16,29 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-  pros::lcd::initialize();
-  pros::lcd::set_text(1, "Hello PROS User!");
-
-  pros::lcd::register_btn1_cb(on_center_button);
+  
   logger.log(INFO, std::string("Hello from " + std::string(__FILE__) +
                                " initalize()"));
-  pros::delay(10);
-  logger.log(INFO, std::string("Hello from " + std::string(__FILE__) +
-                               " initalize() after 10s delay"));
 
-  // test the logger
-  logger.log(DEBUG, "Debug message");
-  logger.log(INFO, "Info message");
-  logger.log(WARNING, "Warning message");
-  logger.log(ERROR, "Error message");
+  logger.info("Hello from initialize()");
+  logger.debug("This is a debug message");
+  logger.warning("This is a warning message");
+  logger.error("This is an error message");
+  logger.error(1);
+  //logger.error("one" + "error");
 
-  // Passing in an int
-  logger.log(INFO, 42);
+  // // Test Data Types
+  // logger.info("Testing data types");
+  // logger.info("int: " + 1);
+  // logger.info("float: " + 1.0f);
+  // logger.info("double: " + 1.0);
+  // logger.info("char: " + 'a');
+  // logger.info("bool: " + true);
+  // logger.info("string: " + "Hello, World!");
 
-  // Passing in a double
-  logger.log(INFO, 3.14159);
 
-  // Passing in a char
-  logger.log(INFO, 'A');
 
-  // Passing in a string
-  logger.log(INFO, "Hello, World!");
-
-  // Passing in a float
-  logger.log(INFO, 3.14f);
-
-  // Passing in a bool
-  logger.log(INFO, true);
-
-  // Passing in a long
-  logger.log(INFO, 1234567890);
-
-  // Passing in a long long
-  logger.log(INFO, 1234567890123456789);
-
-  // Passing in a long double
-  logger.log(
-      INFO,
-      3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337L);
-
-  // Passing in a std::string
-  logger.log(INFO, std::string("Hello, World!"));
-
-  // Testing no-level log
-  logger.log(std::string("Hello from " + std::string(__FILE__) +
-                         " initalize() but without a default level"));
-
-  // Timing Test
-  // Timing COUT
-  for (int i = 0; i < 1000; i++) {
-    auto start = chrono::high_resolution_clock::now();
-    cout << "Hello, World!" << endl;
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-    logger.log(INFO,
-               "COUT Time: " + to_string(duration.count()) + " microseconds");
-  }
-
-  // Timing Logger
-  for (int i = 0; i < 1000; i++) {
-	auto start = chrono::high_resolution_clock::now();
-	logger.log(INFO, "Hello, World!");
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-	logger.log(INFO,
-			   "Logger Time: " + to_string(duration.count()) + " microseconds");
-  }
 }
-
 /**
  * Runs while the robot is in the disabled state of Field Management System or
  * the VEX Competition Switch, following either autonomous or opcontrol. When
@@ -149,21 +84,5 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-  pros::Controller master(pros::E_CONTROLLER_MASTER);
-  pros::Motor left_mtr(1);
-  pros::Motor right_mtr(2);
 
-  while (true) {
-    pros::lcd::print(0, "%d %d %d",
-                     (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-                     (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-                     (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-    int left = master.get_analog(ANALOG_LEFT_Y);
-    int right = master.get_analog(ANALOG_RIGHT_Y);
-
-    left_mtr = left;
-    right_mtr = right;
-
-    pros::delay(20);
-  }
 }
